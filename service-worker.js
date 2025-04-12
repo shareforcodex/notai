29eae5a13cb7370d13d1d9e25bd5785cb3ade02b
Your Name <you@example.com>
2025-04-12 18:04:35 +0800

    refactor: simplify service worker caching logic and improve fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,24 @@
 // service-worker.js
-
-let cacheList = [
-  './',
-  './styles.css',
-  './script.js',
-  './icons/notai-192x192.png',
-  './icons/notai-512x512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
-];
-let cacheName = 'app-cache';
-
-
 self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('app-cache').then(cache => {
-        return cache.addAll(cacheList);
+        return cache.addAll([
+          './',
+          './index.html',
+          './styles.css',
+          './script.js',
+          './icons/notai-192x192.png',
+          './icons/notai-512x512.png',
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+        ]);
       })
     );
   });
   
   self.addEventListener('fetch', event => {
-    //check if the request is in the cachelist
-    let isInCacheList ;
-    if(cacheList.includes(event.request.url)){
-      isInCacheList = true;
-    }
-    else{
-      isInCacheList = false;
-    }
-    //if the request is in the cachelist, return the cached response
-    if (isInCacheList) {
-      event.respondWith(
-        caches.match(event.request).then(response => {
-          //settimeout to update cachelist everytime
-          setTimeout(() => {
-            //check if cachelist is in cache
-            caches.open(cacheName).then(cache => {
-              cache.add(event.request).then(() => {
-                console.log('Cache updated');
-              });
-            });
-          }, 1000); 
-          
-          //if response is found in cache, return it, if not, fetch from network and cache it then return it
-          return response || fetch(event.request).then(response => {
-            return caches.open(cacheName).then(cache => {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          });
-        })
-      );
-    }
-    else{
-      //if the request is not in the cachelist, return the network response
     event.respondWith(
       caches.match(event.request).then(response => {
         return response || fetch(event.request);
       })
     );
-    }
-
-    
   });
